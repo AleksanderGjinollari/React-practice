8b0ec855c2c8d1ebae9bba3076b02cf4f1ff3792
Aleksander Gjinollari <aleksandergjinollari@hotmail.com>
2022-06-02 14:22:53 +0200

    adding a component

--- src/index.js
+++ src/index.js
@@ -1,26 +1,16 @@
 // Import the React and ReactDOM libraries
 import React from "react";
 import ReactDOM from "react-dom";
-import faker from "faker";
+import CommentDetail from "./CommentDetail";
 
 // Create a react component
 const App = () => {
   return (
     <div className="ui container comments">
-      <div className="comment">
-        <a href="/" className="avatar">
-          <img alt="avatar" src={faker.image.image()} />
-        </a>
-        <div className="content">
-          <a href="/" className="author">
-            Sam
-          </a>
-          <div className="metadata">
-            <span className="date">Today at 6:00 PM</span>
-          </div>
-          <div className="text">Nice blog post!</div>
-        </div>
-      </div>
+      <CommentDetail />
+      <CommentDetail />
+      <CommentDetail />
+      <CommentDetail />
     </div>
   );
 };
