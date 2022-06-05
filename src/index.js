99fce74137094fb0e786fd8b1a5cc63d51881631
Aleksander Gjinollari <aleksandergjinollari@hotmail.com>
2022-06-05 19:13:48 +0200

    component reuse

--- src/index.js
+++ src/index.js
@@ -9,6 +9,8 @@ import ApprovalCard from "./ApprovalCard";
 const App = () => {
   return (
     <div className="ui container comments">
+      <ApprovalCard>Are you sure you want to do this?</ApprovalCard>
+
       <ApprovalCard>
         <CommentDetail
           author="Sam"
@@ -18,24 +20,32 @@ const App = () => {
         />
       </ApprovalCard>
 
-      <CommentDetail
-        author="Alex"
-        timeAgo="Today at 2:15PM"
-        comment="Amazing"
-        avatar="https://cdn-icons.flaticon.com/png/512/2042/premium/2042898.png?token=exp=1654282560~hmac=85b7bf3dd80625382067dcd8a3ae2460"
-      />
-      <CommentDetail
-        author="Jane"
-        timeAgo="Today at 6:41PM"
-        comment="Wow, best thing I've seen today"
-        avatar="https://cdn-icons.flaticon.com/png/512/2042/premium/2042907.png?token=exp=1654282578~hmac=44d0b97c10194f87c49c5984d866fd33"
-      />
-      <CommentDetail
-        author="Efi"
-        timeAgo="Yesterday at 1:45AM"
-        comment="Meh..."
-        avatar="https://cdn-icons-png.flaticon.com/512/168/168734.png"
-      />
+      <ApprovalCard>
+        <CommentDetail
+          author="Alex"
+          timeAgo="Today at 2:15PM"
+          comment="Amazing"
+          avatar="https://cdn-icons.flaticon.com/png/512/2042/premium/2042898.png?token=exp=1654282560~hmac=85b7bf3dd80625382067dcd8a3ae2460"
+        />
+      </ApprovalCard>
+
+      <ApprovalCard>
+        <CommentDetail
+          author="Jane"
+          timeAgo="Today at 6:41PM"
+          comment="Wow, best thing I've seen today"
+          avatar="https://cdn-icons.flaticon.com/png/512/2042/premium/2042907.png?token=exp=1654282578~hmac=44d0b97c10194f87c49c5984d866fd33"
+        />
+      </ApprovalCard>
+
+      <ApprovalCard>
+        <CommentDetail
+          author="Efi"
+          timeAgo="Yesterday at 1:45AM"
+          comment="Meh..."
+          avatar="https://cdn-icons-png.flaticon.com/512/168/168734.png"
+        />
+      </ApprovalCard>
     </div>
   );
 };
